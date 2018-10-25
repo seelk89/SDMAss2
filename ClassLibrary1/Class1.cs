f1378786cd67efac5b94f766b787d1eb61d5106a
seelk89 <seelk.89@gmail.com>
2018-10-25 10:10:06 +0200

    Initial commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        //C:\Brugere\Jesper\Skrivebord\ratings.json
+    }
+}
---
+++ ClassLibrary1/Interface1.cs
@@ -0,0 +1,7 @@
+namespace ClassLibrary1
+{
+    public interface Interface1
+    {
+        
+    }
+}
